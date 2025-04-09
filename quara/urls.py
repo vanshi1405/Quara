48200f26c77189a4bcaf97e5377c389e7b65d47e
vanshi1405 <vanshi1405shah@gmail.com>
2025-04-09 22:32:44 +0530

    django viewset connect pending

--- quara/urls.py
+++ quara/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import QuestionViewset, AnswerViewset, LikeViewSet, signup_view, login_view
+from .views import QuestionViewset, AnswerViewset, LikeViewSet
 
 router = DefaultRouter()
 router.register(prefix=r'questions', viewset=QuestionViewset, basename='question')
@@ -9,6 +9,4 @@ router.register(prefix=r'likes', viewset=LikeViewSet, basename='like')
 
 urlpatterns = [
     path('', include(router.urls)),
-    # path('signup/', signup_view, name='signup'),
-    # path('login/', login_view, name='login'),
 ]
--- quara/views.py
+++ quara/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from quara.forms import SignUpForm
 from quara.models import Question, Answer, Like
 from quara.serializers import *
 
@@ -25,27 +24,3 @@ class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
     # permission_classes = [IsAuthenticated]
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('/')
-    return render(request, 'core/login.html')
-
-def signup_view(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Auto login after signup
-            return redirect('home')  # Redirect to homepage
-    else:
-        form = SignUpForm()
-    return
-
-def login_view(request):
-    pass
